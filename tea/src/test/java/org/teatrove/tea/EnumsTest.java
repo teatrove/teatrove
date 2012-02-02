0f4afd93ae4750439321094ff38a17cefc140b03
Nicholas Hagen <nicholas.hagen@znetdevelopment.com>
2012-02-02 15:11:50 -0500

    - fixing #23 to address downcasting in assignment operator - adding enumeration test

---
+++ tea/src/test/java/org/teatrove/tea/EnumsTest.java
@@ -0,0 +1,63 @@
+package org.teatrove.tea;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class EnumsTest extends AbstractTemplateTest {
+
+    @Before
+    public void setup() {
+        addContext("EnumsApplication", new EnumsContext());
+    }
+
+    @Test
+    public void testValues() throws Exception {
+        assertEquals("0", executeSource(TEST_ENUM_SOURCE_1));
+        assertEquals("INVALID", executeSource(TEST_ENUM_SOURCE_2));
+        assertEquals("VALID", executeSource(TEST_ENUM_SOURCE_3));
+        assertEquals("VALID", executeSource(TEST_ENUM_SOURCE_4));
+        assertEquals("true", executeSource(TEST_ENUM_SOURCE_5));
+        assertEquals("true", executeSource(TEST_ENUM_SOURCE_6));
+        assertEquals("blah", executeSource(TEST_ENUM_SOURCE_7));
+    }
+
+    protected static final String TEST_ENUM_SOURCE_1 =
+        "org.teatrove.tea.EnumsTest$TestEnum.VALID.ordinal()";
+    
+    protected static final String TEST_ENUM_SOURCE_2 =
+        "org.teatrove.tea.EnumsTest$TestEnum.INVALID.name()";
+    
+    protected static final String TEST_ENUM_SOURCE_3 =
+        "org.teatrove.tea.EnumsTest$TestEnum.values()[0].name()";
+    
+    protected static final String TEST_ENUM_SOURCE_4 =
+        "org.teatrove.tea.EnumsTest$TestEnum.valueOf('VALID').name()";
+    
+    protected static final String TEST_ENUM_SOURCE_5 =
+        "a = getTestEnum(0); if (a == org.teatrove.tea.EnumsTest$TestEnum.VALID) { 'true' }";
+    
+    protected static final String TEST_ENUM_SOURCE_6 =
+        "a = getTestEnum(1); if (a == org.teatrove.tea.EnumsTest$TestEnum.INVALID) { 'true' }";
+    
+    protected static final String TEST_ENUM_SOURCE_7 =
+        "org.teatrove.tea.EnumsTest$TestEnum.VALID.test()";
+
+    // TODO: if both left and right is enum, only do ==
+    
+    public static enum TestEnum {
+        VALID,
+        INVALID;
+        
+        public String test() { return "blah"; }
+    }
+    
+    public static class EnumsContext {
+       public TestEnum getTestEnum(int value) {
+           return TestEnum.values()[value];
+       }
+    }
+}
